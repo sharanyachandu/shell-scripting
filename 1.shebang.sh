0909e7779e41be01762d21b34d80a4887dc7db55
sharanya chandu <you@example.com>
2023-03-13 20:02:17 +0530

    auto push

---
+++ 1.shebang.sh
@@ -0,0 +1,5 @@
+#bin/bash
+in everyfirst line with # is the shebang notation which tells which interpreteur we are using like java,python or shell/bash
+Except first line everthing with # is considered as comment 
+
+echo hello world
