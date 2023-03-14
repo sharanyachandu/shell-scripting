af9c55bebc1faeccd2ff49fac7cfc867abc9a37a
sharanya chandu <you@example.com>
2023-03-14 20:56:43 +0530

    auto save

--- 6.specialvariables.sh
+++ 6.specialvariables.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 #speacial characters $0-$9,$*,$#,$$
+#$*,$@ prints the used variables
+#echo $$ prints the current processId
+#$# gives number of arguments that are used
+
 a=10
 echo "value of a is $a"
 echo "name of the script is :$0"
@@ -9,3 +13,4 @@ echo first value is $2
 echo first value is $3
 #bash scriptname.sh val1 val2 val3 till 9
 echo "supplied variables are: $* "
+echo "no of arguments that are used: $#"
