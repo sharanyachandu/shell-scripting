450e0310802d2ce84484685b83b3644274e6c89d
sharanya chandu <you@example.com>
2023-03-14 19:24:29 +0530

    auto save

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+#In bash their is no concept of variable declaRATION LIKE INT,FLOAT,string
+#varibles can be declared using $ ex echo $a
+#echo $a
+#echo ${a}
+#echo "$a"
+a=10
+b=7
+echo${b}
+echo "$a"
+echo $b
