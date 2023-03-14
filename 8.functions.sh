803f56c0ee6a6e61752d4c6c26aad280e59a6e67
sharanya chandu <you@example.com>
2023-03-14 23:05:39 +0530

    auto save

---
+++ 8.functions.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+#there are 4 types of commands available
+#1.binary   (/bin ,/sbin)
+#2.Aliases  (alias are shotcuts,alias net="netstat -tulpn")
+#3.shell-builtin commands
+#4.Functions functions are commands that can be written in sequence and called n number of times
+
+#declaring a function
+ #ex:function(){
+  #  echo""msg to be called"
+# } To call function in commad line syntax:function
+#ex
+sample(){
+    echo "This is the sample function to call"
+}
+#THIS IS HOW WE CALL A FUNCTION
+    sample
