e9934e7ffc0689bb39756f0436cf9d77f5111a69
sharanya chandu <you@example.com>
2023-03-14 20:41:17 +0530

    auto save

--- 6.specialvariables.sh
+++ 6.specialvariables.sh
@@ -1,7 +1,11 @@
 #!bin/bash
 #speacial characters $0-$9,$*,$#,$$
+a=10
+echo "value of a=$a"
 echo "name of the script is :$0"
 echo first value is $1
 
 echo first value is $2
 echo first value is $3
+#bash scriptname.sh val1 val2 val3 till 9
+echo "supplied variables are:$*"
