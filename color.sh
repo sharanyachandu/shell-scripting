a08fa40ff5b0b57a275e418d019d2bcd9350e3b1
sharanya chandu <you@example.com>
2023-03-14 19:02:44 +0530

    auto save

---
+++ color.sh
@@ -0,0 +1,17 @@
+#bin/bash
+
+#Color	Foreground	Background
+#Black	30m	\033[40m
+#Red	\033[31m	\033[41m
+#Green	\033[32m	\033[42m
+#Orange	\033[33m	\033[43m
+#Blue	\033[34m	\033[44m
+#Magenta	\033[35m	\033[45m
+#Cyan	\033[36m	\033[46m
+#Light gray	\033[37m	\033[47m
+#Fallback to distro's default	\033[39m	\033[49m
+
+#SYNTAX:
+#    echo -e "\ecolorcodem msg to print \e[0m"
+ echo -e "\e31m this is red color \e[0m"
+
