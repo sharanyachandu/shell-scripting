32a0b81bf3439052417bd5bf79abe73f113565c3
sharanya chandu <you@example.com>
2023-03-14 20:00:14 +0530

    auto save

--- dynamic.sh
+++ dynamic.sh
@@ -1,3 +1,7 @@
 #!bin/bash
 todaydate="14-03-23"
+echo -e "today date is ${todaydate}" #this is called hardcoding
+#To know the exact date we can use option {date +%F}
+#ex
+todaydate="date +%F"
 echo -e "today date is ${todaydate}"
