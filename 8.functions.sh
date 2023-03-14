5eb8ee9ab82632e4afb3523e715d94611a0fd0a5
sharanya chandu <you@example.com>
2023-03-14 23:31:29 +0530

    auto save

--- 8.functions.sh
+++ 8.functions.sh
@@ -17,7 +17,7 @@ sample(){
     #sample
 
     test(){
-        echo"Total number of sessions:$(who | wc -l)"
-        echo "Todays date is :$(date+%F)"
+        echo"Total number of sessions: $(who | wc -l)"
+        echo "Todays date is: $(date +%F)"
     }
     test
