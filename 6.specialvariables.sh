b65bed8b4c64b10503013600966c5821beaacf53
sharanya chandu <you@example.com>
2023-03-14 20:58:45 +0530

    auto save

--- 6.specialvariables.sh
+++ 6.specialvariables.sh
@@ -8,7 +8,7 @@ a=10
 echo "value of a is $a"
 echo "name of the script is :$0"
 echo first value is $1
-
+echo "processid:$$"
 echo first value is $2
 echo first value is $3
 #bash scriptname.sh val1 val2 val3 till 9
