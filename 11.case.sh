53ea5a3ac7b32628408aab04be9d7d31778ad8af
sharanya chandu <you@example.com>
2023-03-15 12:06:44 +0530

    auto save

--- 11.case.sh
+++ 11.case.sh
@@ -1,10 +1,19 @@
 #/bin/bash
 #case condition
+
 ACTION=$1
+
+if [ -z "$1" ]
+then
+    echo "No argument passed."
+else
+    echo "Argument passed: $1"
+fi
+
 
 case $ACTION in
 start)
-        echo "starting payment service"
+         echo "starting payment service"
 ;;
 stop)
         echo "stoping payment process"
