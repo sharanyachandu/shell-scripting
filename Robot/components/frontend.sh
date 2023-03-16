094953e975f4b0ce8ba435d3a06825237b701954
sharanya chandu <you@example.com>
2023-03-16 18:30:01 +0530

    auto save

--- Robot/components/frontend.sh
+++ Robot/components/frontend.sh
@@ -1,6 +1,8 @@
  #!bin/bash
 
 set -e
+    COMPONENT=frontend
+    LOG-FILE="/tmp/frontend.log"
 #validating whether the executed user is root user or not
 
 ID=$(id -u)
@@ -9,26 +11,41 @@ if [ "$ID" -ne 0 ] ; then
     exit 1
 fi 
 
-echo  -n "installing nginx" 
-yum install nginx -y  &>>/tmp/frontend.log
+stat(){
 if [ "$?" -eq 0 ] ; then
     echo  -e "\e[33m ngnix is succesfully installed \e[0m"
 else
     echo -e "\e[32m ngnix is not installed \e[0m"
 fi
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+}
 
+echo  -n "installing nginx" 
+yum install nginx -y  &>> LOG-FILE
+stat $?
+
+echo "Downloading the $COMPONENT component"
+curl -s -L -o /tmp/f$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+
+echo "performing cleanup of old $COMPONENT component"
 cd /usr/share/nginx/html
- rm -rf *  &>> /tmp/frontend.log
- unzip /tmp/frontend.zip  &>> /tmp/frontend.log
- mv frontend-main/* .
+ rm -rf *  &>> LOG-FILE
+ stat $?
+
+echo "copying the download $COMPONENT content"
+ unzip /tmp/$COMPONENT.zip  &>> LOG-FILE
+ stat $?
+ 
+ mv $COMPONENT-main/* .
  mv static/* .
- rm -rf frontend-main README.md
+ rm -rf $COMPONENT-main README.md
  mv localhost.conf /etc/nginx/default.d/roboshop.conf
+ stat $?
 
- systemctl enable nginx &>> /tmp/frontend.log
- systemctl start nginx  &>> /tmp/frontend.log
 
+ systemctl enable nginx &>> LOG-FILE
+ systemctl start nginx  &>> LOG-FILE
+ stat $?
 # ```
 
 # Let's download the HTDOCS content and deploy it under the Nginx path.
