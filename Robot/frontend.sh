bd684cd4ba00cd83b8fbeca6078e298c7b99acc0
sharanya chandu <you@example.com>
2023-03-16 16:25:03 +0530

    auto save

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -10,19 +10,19 @@ if [ "$ID" -ne 0 ] ; then
 fi 
 
  
-yum install nginx -y
+yum install nginx -y  &>>tmp/frontend.log
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
- rm -rf *
- unzip /tmp/frontend.zip
+ rm -rf *  &>>tmp/frontend.log
+ unzip /tmp/frontend.zip  &>>tmp/frontend.log
  mv frontend-main/* .
  mv static/* .
  rm -rf frontend-main README.md
  mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
- systemctl enable nginx
- systemctl start nginx
+ systemctl enable nginx &>>tmp/frontend.log
+ systemctl start nginx  &>>tmp/frontend.log
 
 # ```
 
