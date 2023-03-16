2c093e86bea5214df88cb1227ad8cbfebe9c8b84
sharanya chandu <you@example.com>
2023-03-16 16:00:07 +0530

    auto save

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -2,15 +2,23 @@
 
 set -e
 #validating whether the executed user is root user or not
-id=$(id -u)
+  
+  id=$(id -u)
 
 if[ "$id" -ne 0 ] ; then
-echo ("execute this as a root user")
-exit 1
+    echo ("execute this as a root user")
+    exit 1
 fi
 yum install nginx -y
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
+cd /usr/share/nginx/html
+ rm -rf *
+ unzip /tmp/frontend.zip
+ mv frontend-main/* .
+ mv static/* .
+ rm -rf frontend-main README.md
+ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
  systemctl enable nginx
  systemctl start nginx
