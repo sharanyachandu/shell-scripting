cb20b7ff2a9c0719c587be774c7ca8516e233628
sharanya chandu <you@example.com>
2023-03-16 16:20:58 +0530

    auto save

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -1,31 +1,28 @@
  #!bin/bash
 
 set -e
+#validating whether the executed user is root user or not
+
 ID=$(id -u)
 if [ "$ID" -ne 0 ] ; then 
     echo -e "\e[31m You should execute this script as a root user or with a sudo as prefix \e[0m" 
     exit 1
 fi 
 
-# #validating whether the executed user is root user or not
-# id=$(id -u)
+ 
+yum install nginx -y
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-# if["$id" -ne 0] ; then
-#     echo ("execute this as a root user")
-# fi
-# yum install nginx -y
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+ rm -rf *
+ unzip /tmp/frontend.zip
+ mv frontend-main/* .
+ mv static/* .
+ rm -rf frontend-main README.md
+ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-# cd /usr/share/nginx/html
-#  rm -rf *
-#  unzip /tmp/frontend.zip
-#  mv frontend-main/* .
-#  mv static/* .
-#  rm -rf frontend-main README.md
-#  mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
-#  systemctl enable nginx
-#  systemctl start nginx
+ systemctl enable nginx
+ systemctl start nginx
 
 # ```
 
