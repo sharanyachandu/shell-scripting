8998d67d8ab474882f9de16f6b73a95c5a136fdb
sharanya chandu <you@example.com>
2023-03-17 11:29:23 +0530

    auto save

--- Robot/components/Mongodb.sh
+++ Robot/components/Mongodb.sh
@@ -1,3 +1,81 @@
 #!bin/bash
+set -e
+    COMPONENT=mongodb
+    LOGFILE="/tmp/$COMPONENT.log"
 
-echo "Iam the MongoDB"
+ID=$(id -u)  #validating whether the executed user is root user or not
+
+if [ "$ID" -ne 0 ] ; then 
+    echo -e "\e[31m You should execute this script as a root user or with a sudo as prefix \e[0m" 
+    exit 1
+fi 
+
+stat(){
+if [ "$?" -eq 0 ] ; then
+    echo  -e "\e[33m sucess \e[0m"
+else
+    echo -e "\e[32m Failure \e[0m"
+fi
+}
+
+echo -n "downloading $COMPONENT repo"
+  curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo
+stat $?
+
+echo "Installing $COMPONENT :"
+  yum install -y mongodb-org <<& /tmp/$COMPONENT.log
+  stat $?
+
+echo "enable and starting $COMPONENT"
+   systemctl enable mongod <<& /tmp/$COMPONENT.log
+   systemctl start mongod <<& /tmp/$COMPONENT.log
+   stat $?
+ 
+# 1. Setup MongoDB repos.
+
+# ```bash
+# # curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo
+# ```
+
+# 1. Install Mongo & Start Service.
+
+# echo -n "installing MongoDB"
+# # yum install -y mongodb-org
+# # systemctl enable mongod
+# # systemctl start mongod
+
+# ```
+
+# 1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
+
+# Config file:   `# vim /etc/mongod.conf`
+
+# ![mongodb-update.JPG](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/87c01042-0f64-4ac4-ae5a-ffaf62836290/mongodb-update.jpg)
+
+# - Then restart the service
+
+# ```bash
+# # systemctl restart mongod
+# ```
+
+  
+
+# - Every Database needs the schema to be loaded for the application to work.
+
+# ---
+
+#       `Download the schema and inject it.`
+
+# ```
+# # curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+# # cd /tmp
+# # unzip mongodb.zip
+# # cd mongodb-main
+# # mongo < catalogue.js
+# # mongo < users.js
+# ```
+
+# Symbol `<` will take the input from a file and give that input to the command.
+
+# - Now proceed with the next component `CATALOGUE`
