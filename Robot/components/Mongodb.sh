47fd477cc30fb6e28562c2281e3dc4903eb85e98
sharanya chandu <you@example.com>
2023-03-17 16:10:38 +0530

    auto save

--- Robot/components/Mongodb.sh
+++ Robot/components/Mongodb.sh
@@ -22,69 +22,35 @@ echo -n "downloading $COMPONENT repo"
   curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo
 stat $?
 
-echo "Installing $COMPONENT :"
+echo -n "Installing $COMPONENT :"
   yum install -y mongodb-org &>> LOGFILE
 stat $?
 
-echo "enable and starting $COMPONENT"
+echo -n "enable and starting $COMPONENT"
    systemctl enable mongod &>> LOGFILE
    systemctl start mongod &>> LOGFILE
 stat $?
  
- echo "updating the $COMPONENT visibility"
+ echo -n "updating the $COMPONENT visibility"
   sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 stat $?
 
-echo "Performing Daemon-reload"
+echo -n "Performing Daemon-reload"
    systemctl daemon-reload &>> LOGFILE
    systemctl restart mongod
 stat $?
 
-# 1. Setup MongoDB repos.
+echo -n "downloading $COMPONENT scheme"
+ curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+stat $?
 
-# ```bash
-# # curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo
-# ```
+echo -n "extracting the scheme"
+unzip $COMPONENT.zip
+stat $?
+ 
+ echo -n "injecting the scheme"
+ cd /tmp/$COMPONEN-main
+ mongo < catalogue.js &>> LOGFILE
+mongo < users.js &>> LOGFILE
+stat $?
 
-# 1. Install Mongo & Start Service.
-
-# echo -n "installing MongoDB"
-# # yum install -y mongodb-org
-# # systemctl enable mongod
-# # systemctl start mongod
-
-# ```
-
-# 1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
-
-# Config file:   `# vim /etc/mongod.conf`
-
-# ![mongodb-update.JPG](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/87c01042-0f64-4ac4-ae5a-ffaf62836290/mongodb-update.jpg)
-
-# - Then restart the service
-
-# ```bash
-# # systemctl restart mongod
-# ```
-
-  
-
-# - Every Database needs the schema to be loaded for the application to work.
-
-# ---
-
-#       `Download the schema and inject it.`
-
-# ```
-# # curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-
-# # cd /tmp
-# # unzip mongodb.zip
-# # cd mongodb-main
-# # mongo < catalogue.js
-# # mongo < users.js
-# ```
-
-# Symbol `<` will take the input from a file and give that input to the command.
-
-# - Now proceed with the next component `CATALOGUE`
