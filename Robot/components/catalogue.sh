37978b1dfa0f8fd00a96acc5e2a92fcc0bb130c3
sharanya chandu <you@example.com>
2023-03-20 10:06:04 +0530

    auto save

--- Robot/components/catalogue.sh
+++ Robot/components/catalogue.sh
@@ -109,83 +109,83 @@ DOWNLOAD_AND_EXTRACT() {
 
 }
 
-NPM_INSTALL() {
+# NPM_INSTALL() {
 
-    echo -n "Installing the $COMPONENT Application :"
-    cd /home/$APPUSER/$COMPONENT/ 
-    npm install &>> $LOGFILE
-    stat $? 
+#     echo -n "Installing the $COMPONENT Application :"
+#     cd /home/$APPUSER/$COMPONENT/ 
+#     npm install &>> $LOGFILE
+#     stat $? 
 
-}
+# }
 
-CONFIG_SVC() {
+# CONFIG_SVC() {
 
-    echo -n "Updating the systemd file with DB Details :"
-    sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/'  -e  's/CARTHOST/cart.roboshop.internal/' -e  's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
-    mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-    stat $? 
+#     echo -n "Updating the systemd file with DB Details :"
+#     sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/'  -e  's/CARTHOST/cart.roboshop.internal/' -e  's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+#     mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+#     stat $? 
 
-    echo -n "Starting the $COMPONENT service : "
-    systemctl daemon-reload &>> $LOGFILE
-    systemctl enable $COMPONENT &>> $LOGFILE
-    systemctl restart $COMPONENT &>> $LOGFILE
-    stat $?
+#     echo -n "Starting the $COMPONENT service : "
+#     systemctl daemon-reload &>> $LOGFILE
+#     systemctl enable $COMPONENT &>> $LOGFILE
+#     systemctl restart $COMPONENT &>> $LOGFILE
+#     stat $?
 
-}
+# }
 
-MVN_PACKAGE() {
-    echo -n "Creating the $COMPONENT Package :"
-    cd /home/$APPUSER/$COMPONENT/ 
-    mvn clean package  &>> $LOGFILE
-    mv target/shipping-1.0.jar shipping.jar
-    stat $?   
-}
+# MVN_PACKAGE() {
+#     echo -n "Creating the $COMPONENT Package :"
+#     cd /home/$APPUSER/$COMPONENT/ 
+#     mvn clean package  &>> $LOGFILE
+#     mv target/shipping-1.0.jar shipping.jar
+#     stat $?   
+# }
 
-PYTHON() {
-    echo -n "Installing Python and dependencies :"
-    yum install python36 gcc python3-devel -y  &>> $LOGFILE
-    stat $?
+# PYTHON() {
+#     echo -n "Installing Python and dependencies :"
+#     yum install python36 gcc python3-devel -y  &>> $LOGFILE
+#     stat $?
 
-    # Calling Create-User Functon 
-    CREATE_USER
+#     # Calling Create-User Functon 
+#     CREATE_USER
 
-    # Calling Download_And_Extract Function
-    DOWNLOAD_AND_EXTRACT
+#     # Calling Download_And_Extract Function
+#     DOWNLOAD_AND_EXTRACT
 
-    echo -n "Installing $COMPONENT :"
-    cd /home/roboshop/$COMPONENT/ 
-    pip3 install -r requirements.txt   &>> $LOGFILE 
-    stat $? 
+#     echo -n "Installing $COMPONENT :"
+#     cd /home/roboshop/$COMPONENT/ 
+#     pip3 install -r requirements.txt   &>> $LOGFILE 
+#     stat $? 
 
-    USERID=$(id -u roboshop)
-    GROUPID=$(id -g roboshop)
+#     USERID=$(id -u roboshop)
+#     GROUPID=$(id -g roboshop)
     
-    echo -n "Updating the $COMPONENT.ini file :"
-    sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  /home/$APPUSER/$COMPONENT/$COMPONENT.ini 
+#     echo -n "Updating the $COMPONENT.ini file :"
+#     sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  /home/$APPUSER/$COMPONENT/$COMPONENT.ini 
 
-    # Calling Config-Svc Function
-    CONFIG_SVC
+#     # Calling Config-Svc Function
+#     CONFIG_SVC
 
-}
+# }
 
-JAVA() {
-    echo -n "Installing Maven  :" 
-    yum install maven -y &>> $LOGFILE
-    stat $?
+# JAVA() {
+#     echo -n "Installing Maven  :" 
+#     yum install maven -y &>> $LOGFILE
+#     stat $?
 
-    # Calling Create-User Functon 
-    CREATE_USER
+#     # Calling Create-User Functon 
+#     CREATE_USER
 
-    # Calling Download_And_Extract Function
-    DOWNLOAD_AND_EXTRACT
+#     # Calling Download_And_Extract Function
+#     DOWNLOAD_AND_EXTRACT
 
-    # Calling Maven Package Functon
-    MVN_PACKAGE
+#     # Calling Maven Package Functon
+#     MVN_PACKAGE
 
-    # Calling Config-Svc Function
-    CONFIG_SVC
+#     # Calling Config-Svc Function
+#     CONFIG_SVC
 
-}
+# }
 
 
 
