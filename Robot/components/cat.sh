7b5d3310230efc42971b639bf4b72dbf7d6c360a
sharanya chandu <you@example.com>
2023-03-20 10:25:42 +0530

    auto save

--- Robot/components/cat.sh
+++ Robot/components/cat.sh
@@ -7,7 +7,7 @@ APPUSER=roboshop
 ID=$(id -u)
 
 if [ "$ID" -ne 0 ] ; then 
-    echo -e "\e[31m You should execute this script as a root user or with a sudo as prefix \e[0m" 
+    echo -e "\e[33m You should execute this script as a root user or with a sudo as prefix \e[0m" 
     exit 1
 fi 
 
