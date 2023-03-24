da9cb661545703dab9644c71a8c674fbc38e09d1
sharanya chandu <you@example.com>
2023-03-24 09:07:18 +0530

    auto save

--- Robot/components/create-ec2.sh
+++ Robot/components/create-ec2.sh
@@ -4,11 +4,18 @@ if [ -z "$1" ]; then
     echo -e "\t\t\t \e[32m Sample Usage is : $ bash create-ec2.sh user \e[0m"
     exit 1
 fi 
+COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images \
     --region us-east-1 \
     --image-ids DevOps-LabImage-CentOS7)
     echo -n "AMI_ID is $AMI_ID"
+
+
+echo -n "Launching the instance with $AMI_ID as AMI :"
+
+   aws ec2run-instance --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
+
     # This is a script created to launch EC2 Servers and create the associated Route53 Record 
 
 
