401bb1730c5e2a93bd2536668834b26bc980137f
sharanya chandu <you@example.com>
2023-03-28 12:33:33 +0530

    auto save

--- Robot/components/create-ec2.sh
+++ Robot/components/create-ec2.sh
@@ -12,7 +12,7 @@ HOSTEDZONEID="Z033809539GY1GW64NYAV"
 COMPONENT=$1 
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-allow-all-sg  | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=batch_53 | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 
 echo -n "Ami ID is $AMI_ID"
 
